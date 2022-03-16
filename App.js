40a20d92722e6b00924f0e705d58645765efe1ad
Aleksandr232 <aleksrowing232@gmail.com>
2022-03-16 22:02:42 +0300

    app

--- App.js
+++ App.js
@@ -1,14 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet,  View, TextInput, ImageBackground, Text } from 'react-native';
+
+const api = {
+  key: "af3904bcfb9954b533100c6413793863",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 export default function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  const imge ={uri :'https://raw.githubusercontent.com/Aleksandr232/weather/master/src/assets/cold-bg.jpg'}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <View style={styles.container}  >
+          <ImageBackground source={imge} style={{width: '100%', height: '100%'}}>
+              <TextInput
+              type="text"
+              style={{ color: "black",
+              flex: 1,
+              width:'100%',
+              height: 100,
+              fontSize: 20,
+              fontWeight: "bold",
+              textAlign: "center",
+              }}
+              placeholder="Search..."
+              onChange={e => setQuery(e.target.value)}
+              value={query}
+              onKeyPress={()=>search}
+              />
+              <Text></Text>
+          </ImageBackground>
+        </View>
+        
+      
+     
+         
+    
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +74,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
+  
 });
